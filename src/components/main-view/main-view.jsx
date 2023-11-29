4777f6782b947d1e72d704e31dc4c51e916391ff
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-11-29 23:53:55 +0100

    login-view folder added

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,17 +1,34 @@
 import { useState, useEffect } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
+
 
 export const MainView = () => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
     const [movies, setMovies] = useState([
     ]);
 
     const [selectedMovie, setSelectedMovies] = useState(null);
+    const [user, setUser] = useState(storedUser? storedUser : null);
+    const [token, setToken] = useState(storedToken? storedToken : null);
 
     useEffect(() => {
-        fetch("https://myflix-922o.onrender.com")
-            .then((response) => response.json())
-            .then((data) => {
+        if (!token) 
+            return;
+        
+        fetch("https://myflix-922o.onrender.com/movies",
+            {
+            headers: { Authorization: `Bearer ${token}` },
+    })
+        .then((response) => response.json())
+        .then((movies) => {
+            console.log(movies);
+        });
+    }, [token]);
+    
                 const moviesFromApi = data.docs.map((doc) => {
                     return {
                         id: movie.id,
@@ -24,8 +41,21 @@ export const MainView = () => {
                     };
                 });
                 setMovies(moviesFromApi);
-        })
-    })
+            };
+    
+
+    if (!user) {
+        return (
+            <>
+            <LoginView onLoggedIn={(user, token) => {
+            setUser(user);
+            setToken(token);
+        }} />
+            or
+            <SignupView />
+                </>
+        );
+    }
 
     if (selectedMovie) {
         return (
@@ -40,6 +70,11 @@ export const MainView = () => {
 
     return (
         <div>
+            <button onClick={() => {
+                setUser(null); setToken(null);
+                localStorage.clear();
+            }}>Logout</button>
+ 
             {movies.map((movie) => (
                 <MovieCard
                     key={movie.id}
@@ -51,5 +86,4 @@ export const MainView = () => {
             ))}
             
         </div>
-    );
-};
+           );
