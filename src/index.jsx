c7c1d1de299a35c1fed685a112f1dbebf2b706d4
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-12-08 16:51:40 +0100

    info updated

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { MainView } from './components/main-view/main-view';
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
-import { container } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 // Main component (will eventually use all the others)
 const MyFlixApplication = () => {
