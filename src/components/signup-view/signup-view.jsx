56d91f5e2dce02fb467d342d3ceb26b3f5f6aa94
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-12-08 17:00:12 +0100

    info updated

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "react-bootstrap/Button";
-import { Form } from "react-bootstrap/Form";
+import { Row } from "react-bootstrap/Row";
+import { Col } from "react-bootstrap/Col"
 
 
 export const SignupView = () => {
