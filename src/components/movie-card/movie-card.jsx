0a34b71a3bc524c06c35e0c67e00f233ed3e3824
ossyogwua <132332856+ossyogwua@users.noreply.github.com>
2023-12-20 08:43:19 +0100

    fixed fav movies listing

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,119 +1,72 @@
 import React from "react";
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 import "./movie-card.scss";
 import { Button, Card, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 
-export const MovieCard = ({ movie, token, setUser, user}) => {
+export const MovieCard = ({
+  movie,
+  user,
+  addFavoriteMovie,
+  removeFavoriteMovie,
+}) => {
+  const [isFavorite, setIsFavorite] = useState(false);
 
-    const [isFavorite, setIsFavorite] = useState(
-        false
-      )
-      
+  useEffect(() => {
+    if (user.favoriteMovies && user.favoriteMovies.includes(movie._id)) {
+      isFavorite(true);
+    }
+  }, [user]);
 
-    useEffect(() => {
-        if (user.favoriteMovies && user.favoriteMovies.includes(movie._id)) {
-          isFavorite(true);
-        }
-     }, [user]);
-    
-      const addFavoriteMovie = () => {
-        fetch(
-          `https://myflix-922o.onrender.com/users/${user.name}/movies/${movie._id}`,
-          { method: "POST", headers: { Authorization: `Bearer ${token}` } }
-        )
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            } else {
-              console.log("Failed to add fav movie");
-            }
-          })
-          .then((user) => {
-            if (user) {
-              alert("successfully added to favorites");
-              localStorage.setItem("user", JSON.stringify(user));
-              setUser(user);
-              isFavorite(true);
-            }
-          })
-          .catch((error) => {
-            alert(error);
-          });
-      };
-    
-      const removeFavoriteMovie = () => {
-        fetch(
-          `https://myflix-922o.onrender.com/users/${user.Username}/movies/${movie._id}`,
-          { method: "DELETE", headers: { Authorization: `Bearer ${token}` } }
-        )
-          .then((response) => {
-            if (response.ok) {
-              return response.json();
-            } else {
-              alert("Failed");
-            }
-          })
-          .then((user) => {
-            if (user) {
-              alert("successfully deleted from favorites");
-              localStorage.setItem("user", JSON.stringify(user));
-              setUser(user);
-              isFavorite(false);
-            }
-          })
-          .catch((error) => {
-            alert(error);
-          });
-      };
-    
-    return (
-        <Card className="h-100 mt-5 card-shadow">
-                <Card.Img variant="top card-img" src={movie.ImagePath} />
-                <Card.Body>
-                <Card.Title>{movie.Title}</Card.Title>
-                <Card.Text>{movie.Director.Name}</Card.Text>
-                <Card.Text>{movie.Description}</Card.Text>
-                <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-                    <Button varient="link">Open</Button>
-
-                    
-                    <div>
-                    {isFavorite ? (
-                            <Button className="fav-button" onClick={() => removeFavoriteMovie(movie._id)}>
-                                Remove
-                                </Button>
-                    ) : (
-                                <Button className="fav-button" onClick={() => addFavoriteMovie(movie._id)}>
-                                    Add
-                                    </Button>
-                                
-                    )}
-                </div>
-                
-                    
-                     </Link>
-            </Card.Body>
-            
-            
-                
-        </Card>
-
-
-        );
+  return (
+    <Card className="h-100 mt-5 card-shadow">
+      <Card.Img variant="top card-img" src={movie.ImagePath} />
+      <Card.Body>
+        <Card.Title>{movie.Title}</Card.Title>
+        <Card.Text>{movie.Director.Name}</Card.Text>
+        <Card.Text>{movie.Description}</Card.Text>
+        <div className="btn-container">
+          <div>
+            <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+              <button className="back-button" style={{ cursor: "pointer" }}>
+                Open
+              </button>
+            </Link>
+          </div>
+          <div>
+            {user.FavoriteMovies.includes(movie._id) ? (
+              <Button
+                className="fav-button"
+                on
+                onClick={() => removeFavoriteMovie(movie._id)}
+              >
+                <FaHeart />
+              </Button>
+            ) : (
+              <Button
+                className="fav-button"
+                onClick={() => addFavoriteMovie(movie._id)}
+              >
+                <FaRegHeart />
+              </Button>
+            )}
+          </div>
+        </div>
+      </Card.Body>
+    </Card>
+  );
 };
 
-
-
-
-    // define all the props constraints for the MovieCard
+// define all the props constraints for the MovieCard
 MovieCard.propTypes = {
-    movie: PropTypes.shape({
-       // id: PropTypes.string.isRequired,
-        image: PropTypes.string,
-        Title: PropTypes.string,  
-        description: PropTypes.string.isRequired,
-    }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
+  movie: PropTypes.shape({
+    // id: PropTypes.string.isRequired,
+    image: PropTypes.string,
+    Title: PropTypes.string,
+    description: PropTypes.string.isRequired,
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired,
 };
--- src/components/movie-view/movie-view.js
+++ src/components/movie-view/movie-view.js
@@ -6,13 +6,18 @@ import "./movie-view.scss";
 import { Col, Row } from "react-bootstrap";
 import { MovieCard } from "../movie-card/movie-card";
 import { Button, Card } from "react-bootstrap";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 
-export const MovieView = ({ movies }) => {
+export const MovieView = ({
+  movies,
+  addFavoriteMovie,
+  removeFavoriteMovie,
+}) => {
   const { movieId } = useParams();
   const movie = movies.find((movie) => movie._id === movieId);
   const user = JSON.parse(localStorage.getItem("user"));
 
-  console.log(user);
+  console.log(user, movie);
 
   return (
     <Row className="justify-content-md-center one-movie--view " flex="1">
@@ -20,12 +25,12 @@ export const MovieView = ({ movies }) => {
         <img
           src={movie.ImagePath}
           alt="movie cover"
-          className=" movie-view-img"
+          className="movie-view-img"
         />
       </Col>
       <Col md={6} className="col-12">
         <div className="movie-view-text-Title">
-          <span className="h1">Title: </span>
+          <span className="h6">Title: </span>
           <span>{movie.Title}</span>
         </div>
         <div className="movie-view-text-Director">
@@ -40,38 +45,33 @@ export const MovieView = ({ movies }) => {
           <span className="h6">Description: </span>
           <span>{movie.Description}</span>
         </div>
-        <div>
-          {user.FavoriteMovies.includes(movie._id) ? (
-            <Button
-              className="fav-button"
-              on
-              onClick={() => removeFavoriteMovie(movie._id)}
-            >
-              Remove from Favorite
-            </Button>
-          ) : (
-            <Button
-              className="fav-button"
-              onClick={() => addFavorite(movie._id)}
-            >
-              Add to Favorite
-            </Button>
-          )}
+        <div className="btn-container">
+          <div>
+            {user.FavoriteMovies.includes(movie._id) ? (
+              <Button
+                className="fav-button"
+                on
+                onClick={() => removeFavoriteMovie(movie._id)}
+              >
+                <FaHeart />
+              </Button>
+            ) : (
+              <Button
+                className="fav-button"
+                onClick={() => addFavoriteMovie(movie._id)}
+              >
+                <FaRegHeart />
+              </Button>
+            )}
+          </div>
+          <div>
+            <Link to={`/`}>
+              <button className="back-button" style={{ cursor: "pointer" }}>
+                Back
+              </button>
+            </Link>
+          </div>
         </div>
-        <div>
-          <MovieCard
-            movie={movie}
-            removeFavoriteMovie={removeFavoriteMovie}
-            addFavoriteMovie={addFavoriteMovie}
-            isFavorite={user.FavoriteMovies.includes(movie._id)}
-          />
-        </div>
-
-        <Link to={`/`}>
-          <button className="back-button" style={{ cursor: "pointer" }}>
-            Back
-          </button>
-        </Link>
       </Col>
     </Row>
   );
